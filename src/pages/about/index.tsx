6bf59984cb411c82c2430dbb1f3c7de9c1d9d050
SuverBoi <dmitro.kovalik12345@gmail.com>
2023-10-25 19:59:37 +0300

    l

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import React from 'react'
+import Link from 'next/link'
+
+
+
+const index = () => {
+  return (
+    <main>
+      About
+      <Link href="/">Home</Link>
+      <a href="/"></a>
+    </main>
+  )
+}
+
+export default index
